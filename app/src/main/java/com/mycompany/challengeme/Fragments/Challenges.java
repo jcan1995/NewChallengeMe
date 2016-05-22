64de6931128f0829ecbedbd7df5f39adc8047423
Joshua Cantero <cante008@cougars.csusm.edu>
2016-05-21 20:56:19 -0700

    Initial commit

---
+++ app/src/main/java/com/mycompany/challengeme/Fragments/Challenges.java
@@ -0,0 +1,9 @@
+package com.mycompany.challengeme.Fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by joshua on 5/21/2016.
+ */
+public class Challenges extends Fragment {
+}
---
+++ app/src/main/java/com/mycompany/challengeme/LoginActivity.java
@@ -0,0 +1,9 @@
+package com.mycompany.challengeme;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by joshua on 5/21/2016.
+ */
+public class LoginActivity extends AppCompatActivity {
+}
